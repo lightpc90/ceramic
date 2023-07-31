4230e6f01d55fca5fd76c25e5616c51a02b62382
lightpc90 <afoageektech@gmail.com>
2023-07-31 11:34:09 +0100

    added name to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div>
         <Container display='flex' justify='center' alignItems='center' css={{backgroundColor: 'orange', color: 'White', height: '100px', p: '$0'}}>
-            With Love by 24Codelabz
+            With Love by Gideon Abbey (24Codelabz)
         </Container>
     </div>
   )
